a9cf3bd5c13721b3c76a04cab411d5f4e5b3f256
Naroz Ezzat <narozezzat10@gmail.com>
2024-10-14 16:04:44 +0300

    fix login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,29 @@
 import { Button } from "@/components/ui/button";
-import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { Card } from "@/components/ui/card";
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  const { userId } = auth()
-  if (userId != null) redirect("/events")
+  const { userId } = auth();
+  if (userId != null) redirect("/events");
 
-  return <div className="text-center container my-4 mx-auto">
-    <h1 className="text-3xl mb-4">Home Page</h1>
-    <div className="flex gap-2 justify-center">
-      <Button asChild>
-        <SignInButton />
-      </Button>
-      <Button asChild>
-        <SignUpButton />
-      </Button>
-      <UserButton />
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Card className="w-full mx-4 lg:mx-0 lg:w-1/3">
+        <div className="text-center mt-4">
+          <h1 className="text-3xl font-semibold">Login in</h1>
+          <p className="text-gray-400 text-sm">Welcome to Evently!</p>
+        </div>
+        <div className="flex px-4 py-8 flex-col gap-4">
+          <Button asChild>
+            <SignInButton />
+          </Button>
+          <Button asChild>
+            <SignUpButton />
+          </Button>
+        </div>
+      </Card>
     </div>
-  </div>;
+  );
 }
