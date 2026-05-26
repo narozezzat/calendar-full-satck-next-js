94cbc9d6821971bc6a039779d3f72e5156203771
Naroz Ezzat <narozezzat10@gmail.com>
2026-05-26 19:56:29 +0300

    feat: add localized support to ClerkProvider based on next-intl locale

--- src/components/themed-clerk-provider.tsx
+++ src/components/themed-clerk-provider.tsx
@@ -3,14 +3,19 @@
 import * as React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { arSA, enUS } from "@clerk/localizations";
 import { useTheme } from "next-themes";
+import { useLocale } from "next-intl";
 
 export function ThemedClerkProvider({ children }: { children: React.ReactNode }): React.JSX.Element {
   const { resolvedTheme } = useTheme();
   const isDark = resolvedTheme === "dark";
+  const locale = useLocale();
+  const localization = locale === "ar" ? arSA : enUS;
 
   return (
     <ClerkProvider
+      localization={localization}
       appearance={{
         baseTheme: isDark ? dark : undefined,
         variables: {
